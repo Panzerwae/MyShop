29a030fe031dde2d14109b63f6c5ad0455d08674
karelv <karelv@xpedia.co.za>
2020-01-22 14:14:22 +0200

    Started with BasketItemView

---
+++ MyShop/MyShop.Core/ViewModels/BasketItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    class BasketItemViewModel
+    {
+        public String Id { get; set; }
+        public int Quantity { get; set; }
+        public String ProductName { get; set; }
+        public Decimal Price { get; set; }
+        public String Image { get; set; }
+    }
+}
