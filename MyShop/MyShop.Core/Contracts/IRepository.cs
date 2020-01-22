593b188dd5e39ce72098c32941940f0f83595845
karelv <karelv@xpedia.co.za>
2020-01-22 09:22:09 +0200

    Added Unity

---
+++ MyShop/MyShop.Core/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyShop.Core.Models;
+using System.Linq;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> Collection();
+        void Commit();
+        void Delete(string id);
+        T Find(string id);
+        void Insert(T entity);
+        void Update(T entity);
+    }
+}
--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> entities;
@@ -17,7 +18,8 @@ namespace MyShop.DataAccess.InMemory
         {
             this.entityClassName = typeof(T).Name;
             this.entities = cache[this.entityClassName] as List<T>;
-            if (entities == null) {
+            if (entities == null)
+            {
                 entities = new List<T>();
             }
         }
