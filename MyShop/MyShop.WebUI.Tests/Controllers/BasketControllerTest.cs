5a432943cbc609664436aaa5c1eb3a99c48172b0
karelv <karelv@xpedia.co.za>
2020-01-24 12:59:15 +0200

    Working on Unit tests

--- MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Services;
+using MyShop.WebUI.Controllers;
+using MyShop.WebUI.Tests.Mocks;
 
 namespace MyShop.WebUI.Tests.Controllers
 {
@@ -7,8 +13,28 @@ namespace MyShop.WebUI.Tests.Controllers
     public class BasketControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanAddBasketItem()
         {
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpConext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            controller.AddToBasket("1");
+
+            basketService.AddToBasket(httpContext, "1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count());
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
         }
     }
 }
