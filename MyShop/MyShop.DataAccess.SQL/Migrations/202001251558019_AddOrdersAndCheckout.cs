7114cca5790837f1118ebe92c66dcfb3becf4b39
Karel <kpjventer@gmail.com>
2020-01-25 17:59:46 +0200

    Order Checkout

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202001251558019_AddOrdersAndCheckout.cs
@@ -0,0 +1,20 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddOrdersAndCheckout : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OrderItems", "PruductName", c => c.String());
+            DropColumn("dbo.OrderItems", "PrudctName");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.OrderItems", "PrudctName", c => c.String());
+            DropColumn("dbo.OrderItems", "PruductName");
+        }
+    }
+}
